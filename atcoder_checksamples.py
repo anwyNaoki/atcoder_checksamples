19cb807e6845eb9d617c50328b0a585c563cc494
anwyNaoki <40227751+anwyNaoki@users.noreply.github.com>
2019-10-28 15:32:05 +0900

    atcoder_checksamples.py
    20191028 問題の指定方法を変更

--- atcoder_checksamples.py
+++ atcoder_checksamples.py
@@ -1,12 +1,17 @@
 import requests,bs4,sys,subprocess
+import argparse
 
-abcd=sys.argv[1] #どの問題を解くのか（a,b,c,d・・・)
-exed_file=sys.argv[2] 
-contest_name="" 
+parser = argparse.ArgumentParser()
+parser.add_argument("--p_n")
+parser.add_argument("--f")
+parser.add_argument("--c_n")
+args = parser.parse_args()
+print(args)
+abcd = args.p_n  
+contest_name = args.c_n
+exed_file=args.f
 
 #該当ページにアクセスしてhtml取得
-with open("setting") as f:
-    contest_name=f.readline()
 print(contest_name)
 res = requests.get('https://atcoder.jp/contests/'+contest_name+'/tasks/'+contest_name+'_'+abcd)
 res.raise_for_status()
@@ -25,6 +30,7 @@ for i in range(0,len(example)//4):
     print(i)
     with open("tmp","a") as f:
         f.write(example[i*2].rstrip('\r\n').replace('\r',''))
+    
     subprocess.call("python %s < %s >ans"%(exed_file,"tmp"),shell=True)
     with open("ans","r")as f:
         ans=f.read().rstrip('\r\n')
